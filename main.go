cabe2853d055b160e11820189309d7bfb4710e1b
karamveer hooda <karamveer.hooda@gmail.com>
2020-12-29 20:40:42 +0530

    first Go web commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int
+	var y string
+	var z bool
+	//var a int16
+	//var b int32
+	//var c int64
+
+	x = 42
+	y = "James Bond"
+	z = true
+	/*a = 10
+	b = 10
+	c = 10*/
+
+	fmt.Println("x: ", x, "\ty: ", y, "\tz: ", z)
+	fmt.Println("Checking the update in Git")
+	fmt.Printf("\n%T\t%T\t%T", x, y, z)
+
+}
