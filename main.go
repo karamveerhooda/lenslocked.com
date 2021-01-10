004f35cf1cda8867213ca7bbc661d167f7485e8b
karamveer hooda <karamveer.hooda@gmail.com>
2021-01-10 17:13:16 +0530

    Adding Gophercises folder from Jon calhoun

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
@@ -16,9 +15,30 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send me an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
+
+// Exercise : 1 Adding a FAQ page to my applicationunder the path /faq.
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h2>Welcome to FAQ page </h2>")
+}
+
+// Exercise :2 Try setting a custom 404 page by setting your own Notfoundhandler
+//on the Gorilla/mux.Router object.
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry we could not found the page you are looking for! :-(  </h1>")
+}
+
+//
+
 func main() {
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notfound)
 	http.ListenAndServe(":3000", r)
 }
